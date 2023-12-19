a63a6d48ad9405496961cd78cf5c37a30f546855
sebnow91 <sebnow91@gmail.com>
2023-12-19 14:11:53 +0100

    Comment out unsued components

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,14 +1,14 @@
 import { Outlet, Link } from "react-router-dom";
 import "../assets/styles/layout.css";
 
-const spaceOutLinks = (length: number) => {
-  let spaces = "";
-  const ul = document.getElementById("ul");
-  for (let i = 0; i < length; i++) {
-    spaces += "1fr ";
-  }
-  ul!.style.gridTemplateColumns = spaces;
-};
+// const spaceOutLinks = (length: number) => {
+//   let spaces = "";
+//   const ul = document.getElementById("ul");
+//   for (let i = 0; i < length; i++) {
+//     spaces += "1fr ";
+//   }
+//   ul!.style.gridTemplateColumns = spaces;
+// };
 
 const links = ["Home", "Blogs", "Contact"];
 
